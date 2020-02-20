ca2e435a56545b7acd4b05a4d7a18fdcb1da8ab5
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-20 09:52:39 +0100

    update higherOrderFunctions.js

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -26,3 +26,10 @@ Write a function called map which accepts two parameters: an array and a callbac
 The map function should return a new array with the result of each value being 
 passed to the callback function. Here's an example:
 */
+function map(arr, fn){
+    let newArr = [];
+    for (let i=0; i < arr.length; i++){
+        newArr.push(fn(arr[i]));
+    }
+    console.log(newArr);
+}
