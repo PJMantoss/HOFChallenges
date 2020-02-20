05b81f1f76c8551a5c5ac85d80f7debfe0cd1a5c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-20 09:57:25 +0100

    update higherOrderFunctions.js

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -40,4 +40,7 @@ map([1,2,3,4], function(val){
 });
 
 /* Ex. 3
+/* Ex. 3
+Write a function called reject which accepts two parameters an array and a callback. The function should return a new array with all of the values that do not return true to the callback. Here are two examples:
+*/
 */
